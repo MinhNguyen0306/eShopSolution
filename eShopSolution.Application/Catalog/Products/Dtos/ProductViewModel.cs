470327551ab73cee1c5bd0a06c253676863db634
LeMinh <minhnguyen183385@gmail.com>
2022-12-06 22:06:34 +0700

    Dựng cấu trúc Application - Products

---
+++ eShopSolution.Application/Catalog/Products/Dtos/ProductViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Application.Catalog.Products.Dtos
+{
+    public class ProductViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string CategoryName { get; set; }
+        public string CategoryDescription { get; set; }
+    }
+}
